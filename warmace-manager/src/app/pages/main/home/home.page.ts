57a453f4688dc5baa5a3023b68fe1456a3cc71e1
Alvader01 <alfersanchez05@gmail.com>
2025-03-04 13:10:34 +0100

    Finalizadas funcionalidades a falta de cambiar la interfaz

--- warmace-manager/src/app/pages/main/home/home.page.ts
+++ warmace-manager/src/app/pages/main/home/home.page.ts
@@ -1,33 +1,188 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { IonButton, IonContent, IonFab, IonIcon, IonFabButton } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonFab,
+  IonIcon,
+  IonFabButton,
+  IonLabel,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonItem,
+  IonAvatar,
+  IonList,
+  IonChip,
+  IonSkeletonText,
+  IonRefresher,
+  IonRefresherContent, IonCard } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { add } from 'ionicons/icons';
+import { add, createOutline, trashOutline, bodyOutline } from 'ionicons/icons';
+import { Miniature } from 'src/app/models/miniature.model';
+import { User } from 'src/app/models/user.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
+import { SupabaseService } from 'src/app/services/supabase.service';
 import { UtilsService } from 'src/app/services/utils.service';
 import { AddUpdateMiniatureComponent } from 'src/app/shared/components/add-update-miniature/add-update-miniature.component';
 import { HeaderComponent } from 'src/app/shared/components/header/header.component';
+import { QueryOptions } from 'src/app/services/query-options.interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonFabButton, IonIcon, IonFab, IonButton, HeaderComponent, IonContent],
+  imports: [IonCard, 
+    IonRefresherContent,
+    IonRefresher,
+    IonSkeletonText,
+    IonChip,
+    IonList,
+    IonAvatar,
+    IonItem,
+    IonItemSliding,
+    IonItemOptions,
+    IonItemOption,
+    IonLabel,
+    IonFabButton,
+    IonIcon,
+    IonFab,
+    HeaderComponent,
+    IonContent,
+    CommonModule,
+  ],
 })
 export class HomePage implements OnInit {
   firebaseService = inject(FirebaseService);
   utilsService = inject(UtilsService);
-  constructor() { addIcons({add});}
+  supabaseService = inject(SupabaseService);
+  miniatures: Miniature[] = [];
+  loading: boolean = false;
+  constructor() {
+    addIcons({ createOutline, trashOutline, bodyOutline, add });
+  }
 
   ngOnInit() {}
 
-  async signOut() {
-    this.firebaseService.signOut().then(() => {
-      this.utilsService.routerLink('/auth');
-    });
+  getMiniatures() {
+    this.loading = true;
+    const user: User = this.utilsService.getLocalStoredUser()!;
+    const path: string = `users/${user.uid}/miniatures`;
+
+    const queryOptions: QueryOptions = {
+      orderBy: { field: 'strength', direction: 'desc' },
+    };
+
+    let timer: any;
+    const resetTimer = () => {
+      if (timer) clearTimeout(timer);
+      timer = setTimeout(() => {
+        console.log(
+          'No hay más novedades en 5 segundos. Cancelando suscripción.'
+        );
+        sub.unsubscribe();
+        this.loading = false;
+      }, 5000);
+    };
+
+    const sub = this.firebaseService
+      .getCollectionData(path, queryOptions)
+      .subscribe({
+        next: (res: any) => {
+          this.miniatures = res;
+          this.loading = false;
+
+          resetTimer();
+        },
+        error: (error) => {
+          console.error('Error al obtener los datos:', error);
+          this.loading = false;
+
+          if (timer) clearTimeout(timer);
+        },
+      });
+
+    resetTimer();
   }
 
-  addUpdateMiniature() {
-    this.utilsService.presentModal({ component: AddUpdateMiniatureComponent, cssClass: "add-update-modal"})
+  getTotalPower() {
+    return this.miniatures.reduce((accumulator, miniature) =>  accumulator + miniature.strength * miniature.units, 0)
+  }
+
+  async addUpdateMiniature(miniature?: Miniature) {
+    let success = await this.utilsService.presentModal({
+      component: AddUpdateMiniatureComponent,
+      cssClass: 'add-update-modal',
+      componentProps: { miniature },
+    });
+    if (success) {
+      this.getMiniatures();
+    }
+  }
+
+  ionViewWillEnter() {
+    this.getMiniatures();
+  }
+
+  doRefresh(event: any) {
+    setTimeout(() => {
+      this.getMiniatures();
+      event.target.complete();
+    }, 2000);
+  }
+
+  async deleteMiniature(miniature: Miniature) {
+    const loading = await this.utilsService.loading();
+    await loading.present();
+    const user: User = this.utilsService.getLocalStoredUser()!;
+    const path: string = `users/${user.uid}/miniatures/${miniature!.id}`;
+
+    const imagePath = await this.supabaseService.getFilePath(miniature!.image);
+    await this.supabaseService.deleteFile(imagePath!);
+    this.firebaseService
+      .deleteDocument(path)
+      .then(async (res) => {
+        this.miniatures = this.miniatures.filter(
+          (listedMiniature) => listedMiniature.id !== miniature.id
+        );
+        this.utilsService.presentToast({
+          message: 'Mininatura borrada exitosamente',
+          duration: 1500,
+          color: 'success',
+          position: 'middle',
+          icon: 'checkmark-circle-outline',
+        });
+      })
+      .catch((error) => {
+        this.utilsService.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'danger',
+          position: 'middle',
+          icon: 'alert-circle-outline',
+        });
+      })
+      .finally(() => {
+        loading.dismiss();
+      });
+  }
+
+  async confirmDeleteMiniature(miniature: Miniature) {
+    this.utilsService.presentAlert({
+      header: 'Eliminar miniatura',
+      message: '¿Está seguro de que desea eliminar la miniatura?',
+      mode: 'ios',
+      buttons: [
+        {
+          text: 'No',
+        },
+        {
+          text: 'Sí',
+          handler: () => {
+            this.deleteMiniature(miniature);
+          },
+        },
+      ],
+    });
   }
 }
--- warmace-manager/src/app/services/firebase.service.ts
+++ warmace-manager/src/app/services/firebase.service.ts
@@ -1,7 +1,36 @@
 import { Injectable, inject } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, UserCredential, sendPasswordResetEmail } from '@angular/fire/auth';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  UserCredential,
+  sendPasswordResetEmail,
+} from '@angular/fire/auth';
+import {
+  Storage,
+  uploadString,
+  ref,
+  getDownloadURL,
+  deleteObject,
+} from '@angular/fire/storage';
 import { User } from '../models/user.model';
-import { Firestore, setDoc, doc, getDoc } from '@angular/fire/firestore';
+import {
+  Firestore,
+  setDoc,
+  doc,
+  getDoc,
+  addDoc,
+  updateDoc,
+  collection,
+  collectionData,
+  query,
+  deleteDoc,
+  QueryConstraint,
+  orderBy,
+  limit,
+} from '@angular/fire/firestore';
+import { QueryOptions } from './query-options.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -9,21 +38,14 @@ import { Firestore, setDoc, doc, getDoc } from '@angular/fire/firestore';
 export class FirebaseService {
   auth = inject(Auth);
   firestore = inject(Firestore);
+  storage = inject(Storage);
 
   signIn(user: User): Promise<UserCredential> {
-    return signInWithEmailAndPassword(
-        this.auth,
-        user.email,
-        user.password
-      );
-    }
+    return signInWithEmailAndPassword(this.auth, user.email, user.password);
+  }
 
   signUp(user: User): Promise<UserCredential> {
-    return createUserWithEmailAndPassword(
-      this.auth,
-      user.email,
-      user.password
-    );
+    return createUserWithEmailAndPassword(this.auth, user.email, user.password);
   }
 
   async updateUser(displayName: string) {
@@ -44,8 +66,22 @@ export class FirebaseService {
     window.location.reload();
   }
 
+  async isAuthenticated() {
+    const userExists: boolean = await new Promise((resolve) => {
+      const unsubscribe = this.auth.onAuthStateChanged((user) => {
+        unsubscribe();
+        if (user) {
+          resolve(true);
+        } else {
+          resolve(false);
+        }
+      });
+    });
+    return userExists;
+  }
+
   async getDocument(path: string) {
-    const docSnap = await getDoc(doc(this.firestore,path));
+    const docSnap = await getDoc(doc(this.firestore, path));
     return docSnap.data();
   }
 
@@ -53,17 +89,59 @@ export class FirebaseService {
     return setDoc(doc(this.firestore, path), data);
   }
 
-  async isAuthenticated() {
-    const userExists: boolean = await new Promise((resolve) => {
-      const unsubscribe = this.auth.onAuthStateChanged((user) => {
-        unsubscribe();
-        if (user) {
-          resolve (true)
-        } else {
-          resolve (false)
-        }
-      });
+  updateDocument(path: string, data: any) {
+    return updateDoc(doc(this.firestore, path), data);
+  }
+
+  deleteDocument(path: string) {
+    return deleteDoc(doc(this.firestore, path));
+  }
+
+  addDocument(path: string, data: any) {
+    return addDoc(collection(this.firestore, path), data);
+  }
+
+  buildQueryConstraints(options?: QueryOptions): QueryConstraint[] {
+    const queryConstraints: QueryConstraint[] = [];
+
+    // Manejo del orden (orderBy)
+    if (options?.orderBy) {
+      queryConstraints.push(
+        orderBy(options.orderBy.field, options.orderBy.direction)
+      );
+    }
+
+    // Manejo de la cantidad límite (limit)
+    if (options?.limit) {
+      queryConstraints.push(limit(options.limit));
+    }
+
+    return queryConstraints;
+  }
+
+  getCollectionData(path: string, options?: QueryOptions) {
+    const ref = collection(this.firestore, path);
+    const collectionQuery = this.buildQueryConstraints(options);
+    return collectionData(query(ref, ...collectionQuery), { idField: 'id' });
+  }
+
+  async uploadImage(path: string, imageUrl: string) {
+    return uploadString(ref(this.storage, path), imageUrl, 'data_url').then(
+      () => {
+        return getDownloadURL(ref(this.storage, path));
+      }
+    );
+  }
+
+  async getFilePath(url: string) {
+    //return ref(this.storage, url).fullPath
+    const path = await new Promise((resolve) => {
+      resolve('');
     });
-    return userExists;
+    return path as string;
+  }
+
+  async deleteFile(path: string) {
+    return deleteObject(ref(this.storage, path));
   }
 }
